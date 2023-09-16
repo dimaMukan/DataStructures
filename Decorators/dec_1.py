3ff8763d4c34cf38482db1765b641f2a8922cfc8
dimaMukan <ds.mukan@gmail.com>
2023-09-16 13:10:36 +0100

    interesting

--- Decorators/dec_1.py
+++ Decorators/dec_1.py
@@ -1,10 +1,19 @@
-def outer():
-    def div(a,b):
-        return a/b
-    return div
+def outer(func):
+    def inner(*args, **kwargs):
+        print('try')
+        return func(*args, **kwargs)
+
+    return inner
+
+def sum(a,b,c):
+    return a+b+c
+
+def div(a,b):
+    return a/b
 
 
-
-var = outer()
+var = outer(div)
+var1 = outer(sum)
 print(type(var))
 print(var(1,2))
+print(var1(1,2,3))
